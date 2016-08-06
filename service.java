a56c4d0f343d9a928eb5788da0b1ef454a44acd5
E12351 <e12351@ce.pdn.ac.lk>
2016-08-06 23:21:50 +0530

    Connection ending with pub method

--- service.java
+++ service.java
@@ -13,7 +13,6 @@ import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class service extends Service {
